fddf28d3602f3fccda2ce888bd8308389f2b10b9
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2019-09-03 18:26:46 +0200

    Saving token push for user and removing previous uses of this token

--- AndroidApp/app/src/main/java/com/bankmorcillaconf/app/model/User.kt
+++ AndroidApp/app/src/main/java/com/bankmorcillaconf/app/model/User.kt
@@ -1,4 +1,5 @@
 package com.bankmorcillaconf.app.model
 
 data class User(val email: String,
-                val lastConnection: Long)
+                val lastConnection: Long,
+                val tokenPush: String? = null)
