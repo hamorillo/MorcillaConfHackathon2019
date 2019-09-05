d004f71f54cb3c97cf654858cd44e25481d3ebc7
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2019-09-05 09:11:27 +0200

    Sending push from user activity

--- AndroidApp/app/src/main/java/com/bankmorcillaconf/app/UserActivity.kt
+++ AndroidApp/app/src/main/java/com/bankmorcillaconf/app/UserActivity.kt
@@ -3,18 +3,24 @@ package com.bankmorcillaconf.app
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bankmorcillaconf.app.model.Push
+import com.bankmorcillaconf.app.repository.PushRepository
 import com.bankmorcillaconf.app.repository.TaskRepository
 import com.bankmorcillaconf.app.repository.UserRepository
 import com.bankmorcillaconf.app.ui.TasksAdapter
 import com.bankmorcillaconf.app.util.ResultListener
+import com.bankmorcillaconf.app.util.sha1
 import kotlinx.android.synthetic.main.user_activity.*
 
 class UserActivity : AppCompatActivity() {
 
     private val userRepository = UserRepository()
     private val taskRepository = TaskRepository()
+    private val pushRepository = PushRepository()
 
     companion object {
         private const val EXTRA_USER_MAIL = "EXTRA_USER_MAIL"
@@ -66,7 +72,8 @@ class UserActivity : AppCompatActivity() {
         userRepository.getUserMe(userMail, ResultListener(
             onSuccess = {
                 renderMail(it.email)
-                renderStatusIcon(it.currentPomodoroStartDate, it.currentPomodoroDuration)
+                renderStatus(it.currentPomodoroStartDate, it.currentPomodoroDuration)
+                renderCall(it.email)
             },
             onError = {
                 // TODO 2019-09-04 hector
@@ -74,20 +81,44 @@ class UserActivity : AppCompatActivity() {
         ))
     }
 
-    private fun renderStatusIcon(currentPomodoroStartDate: Long?, currentPomodoroDuration: Long?) {
-        statusTextView.setText(
-            if (currentPomodoroStartDate != null && currentPomodoroDuration != null) {
-                val actualTimeStamp = System.currentTimeMillis()
-                val finishPomodoroTimeStamp = currentPomodoroStartDate + currentPomodoroDuration
-                if (actualTimeStamp > finishPomodoroTimeStamp) {
-                    "Now is taking a rest" //getDrawable(R.drawable.user_available_ic)
-                } else {
-                    "Now is working" //getDrawable(R.drawable.user_busy_ic)
-                }
-            } else {
-                "Now is taking a rest" //getDrawable(R.drawable.user_available_ic)
+    private fun renderCall(email: String) {
+        val logedUser = UserRepository.staticUser?.email
+
+        if (logedUser == null || logedUser == email) {
+            sendNotificationImageView.visibility = View.GONE
+        } else {
+            sendNotificationImageView.visibility = View.VISIBLE
+            sendNotificationImageView.setOnClickListener {
+                pushRepository.sendPush(Push(email.sha1(), logedUser.sha1()), ResultListener(
+                    onSuccess = {
+                        Toast.makeText(
+                            this@UserActivity, "Push Sended",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    },
+                    onError = {
+                        Toast.makeText(
+                            this@UserActivity, "Send Push Error",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                ))
             }
-        )
+        }
+    }
+
+    private fun renderStatus(currentPomodoroStartDate: Long?, currentPomodoroDuration: Long?) {
+        statusTextView.text = if (currentPomodoroStartDate != null && currentPomodoroDuration != null) {
+            val actualTimeStamp = System.currentTimeMillis()
+            val finishPomodoroTimeStamp = currentPomodoroStartDate + currentPomodoroDuration
+            if (actualTimeStamp > finishPomodoroTimeStamp) {
+                "Now is taking a rest" //getDrawable(R.drawable.user_available_ic)
+            } else {
+                "Now is working" //getDrawable(R.drawable.user_busy_ic)
+            }
+        } else {
+            "Now is taking a rest" //getDrawable(R.drawable.user_available_ic)
+        }
     }
 
     private fun renderMail(email: String) {
